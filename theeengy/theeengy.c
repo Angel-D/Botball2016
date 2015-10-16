bb73474bf4aa9091e76352936b640d8683a263d6
atomicproton <imim123345667899@hotmail.com>
2015-10-16 15:36:16 -0700

    :tada: The camera now moves up and down

--- theeengy/theeengy.c
+++ theeengy/theeengy.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int main(){
+	int servo;
+	enable_servos();
+	set_servo_position(0, 750);
 	create_connect();
 	printf("Connectedified");
 	camera_open();
@@ -11,17 +14,21 @@ int main(){
 		int y = get_object_center(0,0).y;
 		printf("x value is %d y value is %d\n", x, y);
 		if (get_object_count(0) > 0){
-			/*if (x>40)
-				create_drive_direct(300,100);
-			else if (x>80)
-				create_drive_direct(250,150);
-			else if (x>120)
-				create_drive_direct(150,250);
-			else
-				create_drive_direct(100,300);
-			*/
 			create_drive_direct(140+x,300-x);
+			if (y <= 80) {
+				servo = servo + 30;
+				printf("Up\n");
+			}
+			else {
+				servo = servo - 30;
+				printf ("down");
+			}
 		}
+		if (servo > 1000)
+			servo = 1000;
+		if (servo < 550)
+			servo = 550;
+		set_servo_position( 0, servo);
 		msleep(50);
 	}
 	return 0;
